636263ebabb825848b589f6ba778a029a2e4609b
Alisher <dadabaev027@gmail.com>
2025-08-10 21:07:47 +0500

    add urls

--- news/urls.py
+++ news/urls.py
@@ -1,2 +1,14 @@
 from django.urls import path
-from .views import HomeView, DetailView, AllNewsView, CategoryView, RegionView
+from .views import *
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', HomeView, name="home"),
+    path('all_news/', AllNewsView.as_view(), name="all-news"),
+    path('category/<int:id>', CategoryView, name="category-news"),
+    path('region/<int:id>', RegionView, name="region-news"),
+    path('detail/<int:id>', DetailView, name="detail"),
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
